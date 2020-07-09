f89eaedccaee034577c9543ac936c17b376cbb55
Natrajan <njaiswal78@gmail.com>
2020-07-09 09:15:57 +0530

    laravel

---
+++ blog/resources/views/test.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello Mic Test</title>
+</head>
+<body>
+	<h1 style="position: block;margin-right: auto;margin-left: auto;font-size: 100px;">Hello Mic testing</h1>
+
+</body>
+</html>
